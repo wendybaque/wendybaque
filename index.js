19d2c3941124072a83770e42f66efcfd9b009ad5
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2023-07-21 10:45:08 +0200

    Create index.js
    Try to add a script to update Readme.md

---
+++ index.js
@@ -0,0 +1,75 @@
+const { promises: fs } = require('fs');
+const readme = require('./readme');
+
+const msInOneDay = 1000 * 60 * 60 * 24;
+
+const today = new Date();
+
+function generateNewREADME() {
+  const readmeRow = readme.split('\n');
+
+  function updateIdentifier(identifier, replaceText) {
+    const identifierIndex = findIdentifierIndex(readmeRow, identifier);
+    if (!readmeRow[identifierIndex]) return;
+    readmeRow[identifierIndex] = readmeRow[identifierIndex].replace(
+      `<#${identifier}>`,
+      replaceText
+    );
+  }
+
+  const identifierToUpdate = {
+    // * DBNW = Day Before New Year
+    day_before_new_years: getDBNWSentence(),
+    myself: getMySelf(),
+    today_date: getTodayDate(),
+    gabot_signing: getGabotSigning(),
+  };
+
+  Object.entries(identifierToUpdate).forEach(([key, value]) => {
+    updateIdentifier(key, value);
+  });
+
+  return readmeRow.join('\n');
+}
+
+const moodByDay = {
+  1: 'hate',
+  2: 'wickedness',
+  3: 'pleasure',
+  4: 'wickedness',
+  5: 'cruelty',
+  6: 'horror',
+  7: 'love',
+};
+
+function getGabotSigning() {
+  const mood = moodByDay[today.getDay()];
+  return `🤖 This README.md is updated with ${mood}, by Gabot ❤️`;
+}
+
+function getTodayDate() {
+  return today.toDateString();
+}
+
+function getDBNWSentence() {
+  const nextYear = today.getFullYear() + 1;
+  const nextYearDate = new Date(String(nextYear));
+
+  const timeUntilNewYear = nextYearDate - today;
+  const dayUntilNewYear = Math.round(timeUntilNewYear / msInOneDay);
+
+  return `**${dayUntilNewYear} day before ${nextYear} ⏱**`;
+}
+
+const findIdentifierIndex = (rows, identifier) =>
+  rows.findIndex((r) => Boolean(r.match(new RegExp(`<#${identifier}>`, 'i'))));
+
+const updateREADMEFile = (text) =>
+  fs.writeFile('./README.md', text, () => console.log(text));
+
+function main() {
+  const newREADME = generateNewREADME();
+  console.log(newREADME);
+  updateREADMEFile(newREADME);
+}
+main();
